250db681f9791e3d54785683aa688ec70c04ac5b
gitesh.more <giteshmore.pm@gmail.com>
2026-06-03 15:31:13 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+
+    setTimeout(function () {
+
+        document
+        .getElementById("loader")
+        .classList
+        .add("loader-hide");
+
+    }, 4500);
+
+});
